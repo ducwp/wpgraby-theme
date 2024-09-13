05553c447e7eeb83e21952e074a77427cfd9a533
Đức Nguyễn <ducnv119@gmail.com>
2024-09-13 11:46:43 +0700

    Update index.php

--- index.php
+++ index.php
@@ -7,6 +7,8 @@
           <div class="promo pe-md-3 pe-lg-5">
             <h1 class="headline mb-3">
 
+            <!-- TEST -->
+
               Plugin hỗ trợ lấy bài nhanh<br>cho WordPress
             </h1><!--//headline-->
             <div class="subheadline mb-4">
